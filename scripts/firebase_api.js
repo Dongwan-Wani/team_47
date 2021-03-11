6195a1b10f624ac590acffb1c929f0494b0d2b23
Daniel Zhang <zhangqiangtianchen@gmail.com>
2021-03-10 20:32:54 -0800

    Added firebase api.js and login.html, modified index.html profile.html

---
+++ scripts/firebase_api.js
@@ -0,0 +1,12 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBuO-yRjUE2WoVFJ2qt4dFixREz4i93514",
+    authDomain: "no-more-hesitation.firebaseapp.com",
+    projectId: "no-more-hesitation",
+    storageBucket: "no-more-hesitation.appspot.com",
+    messagingSenderId: "70558095069",
+    appId: "1:70558095069:web:9bd87d03bef949ba68ed34"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
