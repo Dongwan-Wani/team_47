367451e327f959173787d3e97f34c3d1c9f384f1
Jonghoon Jang <jayjang0209@gmail.com>
2021-04-14 23:59:05 -0700

    Add comments and polish code on recommendations.js

--- scripts/recommendations.js
+++ scripts/recommendations.js
@@ -1,21 +1,17 @@
+
+// order dishes by food qulity score and retrieve top three dishes
 function dishQuery() {
     top_three = []
     db.collection("dishes")
-        //.where("population", ">", 1000000)
-        //.where("hemisphere", "==", "south")
-        //.limit(2)
-        //.orderBy("population")
-        .orderBy("foodqualityscore", "desc").limit(3)
+        .orderBy("foodqualityscore", "desc").limit(3)  // retrieve top three dishes in the array in a desc order
         .get()
         .then(function (snap) {
-            // console.log(snap)
             snap.forEach(function (doc) {
                 var dish = doc.data().dish_name;
                 console.log(dish);
                 top_three.push(dish)
-                //document.getElementById("cities-go-here").innerHTML = newdom;
             })
-            readdishInfo(top_three[0], 'first')
+            readdishInfo(top_three[0], 'first') 
             readdishInfo(top_three[1], 'second')
             readdishInfo(top_three[2], 'third')
         })
@@ -23,7 +19,7 @@ function dishQuery() {
 
 }
 
-
+// Read data of dishes and replace text and attr with the data with retrieved from Firestore
 function readdishInfo(dish_name, rank) {
     db.collection("dishes").where("dish_name", "==", dish_name)
         .get()
@@ -33,11 +29,11 @@ function readdishInfo(dish_name, rank) {
                 var restaurant = doc.data().restaurant;
                 var likes = doc.data().foodqualityscore;
                 var picture = doc.data().images;
+
                 let dish_element = `#${rank}-dish-name-goes-here`
                 let rest_element = `#${rank}-restaurant-name-goes-here`
                 let like_element = `#${rank}-likes`
                 let pic_element = `#${rank}-dishPicture`
-                // let rest_a_element = `#${rank}-restaurant-atag-goes-here`
 
                 $(dish_element).text(dish_name);
                 $(rest_element).text(restaurant);
@@ -45,12 +41,7 @@ function readdishInfo(dish_name, rank) {
                 $(pic_element).attr("src", picture);
                 $(rest_element).attr("href", `restaurants_page.html?name=${restaurant}`);
 
-                console.log(dish_name)
-                console.log(restaurant)
-                console.log(likes)
-                console.log(picture)
-                console.log(rank)
-                console.log(pic_element)
+                console.log(dish_name, restaurant, likes, picture, rank, pic_element)
             })
         })
 }
